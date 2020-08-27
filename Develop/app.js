a394d8e72add70c2d200f2be2ee45078bdf6bc0f
Eugenia <eugeniasol13@gmail.com>
2020-08-27 12:38:13 -0400

    working app

--- Develop/app.js
+++ Develop/app.js
@@ -1,4 +1,4 @@
-    const Manager = require("./lib/Manager");
+const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
@@ -9,27 +9,146 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+const allEmployees = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const mainQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your name: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email: "
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "Enter your ID number: "
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Enter manager's office number:",
+        },
+
+    ])
+}
+
+const engineerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter engineer's name: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter engineer's email: "
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "Enter engineer's ID number: "
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter engineer's github username: "
+        },
+
+    ])
+}
+
+const internQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter intern's name: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter intern's email: "
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "Enter intern's ID number: "
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Enter intern's school: "
+        },
+
+    ])
+}
+const selection = () => {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "Would you like to add a new employee?",
+            choices: ['Add Engineer', 'Add Intern', 'Done'],
+        },
+    ])
+
+}
+
+mainQuestions().then((answers) => {
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    allEmployees.push(manager);
+    selectLoop();
+
+});
+
+const selectLoop = () => {
+
+    console.log("SELECT EMPLOYEE TO ADD")
+    selection().then((answers) => {
+        if (answers.choice === "Add Engineer") {
+        
+            engineerQuestions().then((answers) => {
+            storeEng(answers);
+            
+        });
+        } else if (answers.choice === "Add Intern") {
+            internQuestions().then((answers) => {
+            storeIntern(answers);
+            });
+        } else if (answers.choice === "Done") {
+
+
+            fs.writeFileSync(outputPath, render(allEmployees));
+            console.log("You created the employee page!")
+
+
+        }
+    });
+}
+
+const storeEng = (answers) => {
+    
+        allEmployees.push(new Engineer(answers.name, answers.id, answers.email, answers.github))
+        selectLoop();
+    
+}
+
+const storeIntern = (answers) => {
+
+    
+        allEmployees.push(new Intern(answers.name, answers.id, answers.email, answers.school))
+        selectLoop();
+    
+}
